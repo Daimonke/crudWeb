c1e887e8b79508702c9ebe2dc2487fdd1cc05e30
Daimonke <Daimonas98@gmail.com>
2022-03-27 21:24:48 +0300

    Added post method

--- scripts/post.js
+++ scripts/post.js
@@ -4,14 +4,38 @@ const hamburgerClose = document.querySelector('.hamburgerClose')
 const hamburgerIcon = document.querySelector('.hamburgerIcon')
 hamburgerIcon.addEventListener('click', () => {
     navBox.classList.remove('noDisplay')
-    setTimeout(()=> {
-    navBox.classList.remove('navBoxHidden')
+    setTimeout(() => {
+        navBox.classList.remove('navBoxHidden')
     }, 1)
 })
 
 hamburgerClose.addEventListener('click', () => {
     navBox.classList.add('navBoxHidden')
-    setTimeout(()=> {
+    setTimeout(() => {
         navBox.classList.add('noDisplay')
-        }, 500)
+    }, 500)
+})
+
+// Post method
+
+const postForm = document.querySelector('#postForm')
+postForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const data = {
+        "user": "demouser",
+        "brand": e.target.elements.brand.value,
+        "model": e.target.elements.model.value,
+        "fuel": e.target.elements.fuel.value,
+        "image": e.target.elements.image.value,
+        "price": parseInt(e.target.elements.price.value),
+    }
+
+    fetch('http://localhost:3000/cars', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(alert(`Car posted!`))
 })
